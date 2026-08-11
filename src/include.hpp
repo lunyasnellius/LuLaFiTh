5baf6acd0e54925794605557b3753c00610e8f1f
Luna Meyer <lmeyer@uni-bonn.de>
2026-08-11 14:19:54 +0200

    deprecate p-field and momentum objects

--- src/include.hpp
+++ src/include.hpp
@@ -6,7 +6,7 @@
 #include "u1-field.hpp"
 #include "include/functions.hpp"
 #include "u1-action.hpp"
-#include "p-field.hpp"
+// #include "p-field.hpp"
 #include "u1-hamilton.hpp"
 
 
--- src/u1-hamilton.hpp
+++ src/u1-hamilton.hpp
@@ -3,23 +3,23 @@
 #include "include/core.hpp"
 #include "include/functions.hpp"
 #include "u1-field.hpp"
-#include "p-field.hpp"
+// #include "p-field.hpp"
 #include "u1-action.hpp"
 
 
 /*
  * H = 1/2 * pi^2 + S_G
  */
-double u1_hamilton(double beta, u1_field & gauge, p_field & momenta) {
+double u1_hamilton(double beta, u1_field & gauge, u1_field & momenta) {
 	double ham = 0.0;
-	momentum tmp1(0.0);
-	momentum tmp2(0.0);
+	double tmp1(0.0);
+	double tmp2(0.0);
 	// calculate sum of all pi^2
 	for (int x=0; x<8; ++x) {
 		for (int t=0; t<8; ++t) {
-			tmp1 = momenta(t,x,T_DIR) * momenta(t,x,T_DIR);
-			tmp2 = momenta(t,x,X_DIR) * momenta(t,x,X_DIR);
-			ham += tmp1.val + tmp2.val;
+			tmp1 = momenta(t,x,T_DIR).val * momenta(t,x,T_DIR).val;
+			tmp2 = momenta(t,x,X_DIR).val * momenta(t,x,X_DIR).val;
+			ham += tmp1 + tmp2;
 		}
 	}
 	// factor 1/2 for kinetic part
